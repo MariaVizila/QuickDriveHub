ff8abd9004d8a321472f25b90b290689adf8cce5
MariaVizila <knewlon30@gmail.com>
2025-02-13 13:53:47 -0600

    latest_script.lua

--- latest_script.lua
+++ latest_script.lua
@@ -4,35 +4,24 @@ local Window = Library.Main("Test Hub", "C") -- Change "C" to the key you want t
 -- Load DxHook Notify
 local Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/laagginq/ui-libraries/main/dxhooknotify/src.lua", true))()
 
--- Version System
-local currentVersion = "1.0" -- Update this when making a new version
-local latestVersion = game:HttpGet("https://your-raw-github-link/version.txt") -- Store latest version here
+-- Custom Notifications
+Notify:Notify("Welcome to Test Hub", "You are now using Test Hub! Have Fun!", 10)
 
-if latestVersion and latestVersion ~= currentVersion then
-    -- Notify the user about the update
-    Notify:Notify("Update Available", "A new version of Test Hub is available!", 10)
-
-    -- Auto-Update (Optional)
-    local newScript = game:HttpGet("https://your-raw-github-link/latest_script.lua") -- Replace with actual script link
-    loadstring(newScript)()
-    return -- Stop execution of old version
-end
-
--- Tabs & Sections
+-- Tabs & Sections (Using Unique Variable Names)
 local Tab1 = Window.NewTab("Tab 1")
-local Section1 = Tab1.NewSection("Tab 1")
+local Section1 = Tab1.NewSection("Section 1")
 
 local Tab2 = Window.NewTab("Tab 2")
-local Section2 = Tab2.NewSection("Tab 2")
+local Section2 = Tab2.NewSection("Section 2")
 
 local Tab3 = Window.NewTab("Tab 3")
-local Section3 = Tab3.NewSection("Tab 3")
+local Section3 = Tab3.NewSection("Section 3")
 
 local Tab4 = Window.NewTab("Tab 4")
-local Section4 = Tab4.NewSection("Tab 4")
+local Section4 = Tab4.NewSection("Section 4")
 
 local Tab5 = Window.NewTab("Tab 5")
-local Section5 = Tab5.NewSection("Tab 5")
+local Section5 = Tab5.NewSection("Section 5")
 
 local Tab6 = Window.NewTab("Tab 6")
-local Section6 = Tab6.NewSection("Tab 6")
+local Section6 = Tab6.NewSection("Section 6")
