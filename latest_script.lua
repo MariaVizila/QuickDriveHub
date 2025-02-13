217dc2d13e2e61a5453bc7554445078004faf9fd
MariaVizila <knewlon30@gmail.com>
2025-02-13 14:57:29 -0600

    latest_script.lua

--- latest_script.lua
+++ latest_script.lua
@@ -1,12 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/slf0Dev/Ocerium_Project/main/Library.lua"))()
 local Window = Library.Main("Test Hub", "C") -- Change "C" to the key you want to hide the GUI
 
--- Load DxHook Notify
-local Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/laagginq/ui-libraries/main/dxhooknotify/src.lua", true))()
-
--- Custom Notifications
-Notify:Notify("Welcome to Test Hub", "Test Hub Is Loading! Enjoy!", 10)
-
 -- Tabs & Sections (Using Unique Variable Names)
 local Tab1 = Window.NewTab("Tab 1")
 local Section1 = Tab1.NewSection("Section 1")
@@ -25,3 +19,9 @@ local Section5 = Tab5.NewSection("Section 5")
 
 local Tab6 = Window.NewTab("Tab 6")
 local Section6 = Tab6.NewSection("Section 6")
+
+-- Load DxHook Notify
+local Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/laagginq/ui-libraries/main/dxhooknotify/src.lua", true))()
+
+-- Custom Notifications
+Notify:Notify("Welcome to Test Hub", "Test Hub Is Loading! Enjoy!", 10)
